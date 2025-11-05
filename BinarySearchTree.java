a7ef4f91d58893853c45f126cbf69846c6d947b6
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2025-11-05 16:35:34 -0500

    Create BinarySearchTree.java

---
+++ BinarySearchTree.java
@@ -0,0 +1,152 @@
+package tree;
+//Name: Abhiram Ruthala
+//Computing ID: kas4kj
+//Homework Name: HW9-BST
+//Resources Used: Google AI
+
+
+public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> implements Tree<T>{
+
+    @Override
+    public void insert(T data) {
+        this.root = insert(data, root);
+    }
+
+    /**
+     * Helper method for inserting recursively
+     * @param data
+     * @param curNode
+     * @return a reference to the new root of the subtree
+     */
+    protected TreeNode<T> insert(T data, TreeNode<T> curNode) {
+        //TODO: Implement this method
+        if(curNode.data.compareTo(data) <= 0){
+            curNode = curNode.left;
+            insert(data, curNode);
+        } else if (curNode.data.compareTo(data) >= 0){
+            curNode = curNode.right;
+            insert(data, curNode);
+        }
+        return null;
+    }
+
+
+    /**
+     * Returns a boolean (true of false) if the element was found in a BST
+     */
+    @Override
+    public boolean find(T data) {
+        return find(data, root);
+    }
+
+    // Helper method
+    private boolean find(T data, TreeNode<T> curNode) {
+        //TODO: Implement this method
+        if(curNode.data.compareTo(data) <= 0){
+            curNode = curNode.left;
+            find(data, curNode);
+            if(curNode.data.compareTo(data) == 0){
+                return true;
+            } else {
+                return false;
+            }
+        } else if (curNode.data.compareTo(data) >= 0){
+            curNode = curNode.right;
+            find(data, curNode);
+            if(curNode.data.compareTo(data) == 0){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Returns the max item in the given subtree
+     */
+    public T findMax() {
+        return findMax(this.root);
+    }
+
+    // Helper method
+    private T findMax(TreeNode<T> curNode) {
+        //TODO: Implement this method
+        if (curNode.right != null) {
+            curNode = curNode.right;
+            findMax(curNode);
+            return curNode.data;
+        }
+        return null;
+    }
+
+    //-----------------------------------------------------------------------------
+    //THE METHOD BELOW IS PARTIALLY IMPLEMENTED    
+    //YOU NEED TO COMPLETE THE IMPLEMENTATION
+    //-----------------------------------------------------------------------------
+
+    @Override
+    public void remove(T data) {
+        this.root = remove(data, root); // Call remove at the root of the tree
+    }
+
+    protected TreeNode<T> remove(T data, TreeNode<T> curNode) {
+        /* Note the use of compareTo() in the solution! */
+
+	// first check if the node to be inserted is null, if so return the node
+        if(curNode == null) return curNode;
+
+
+        // if item I want to remove is smaller than the data of the current node...
+        else if (data.compareTo(curNode.data) < 0) {
+            // recursively call remove on LEFT subtree
+	    /* TODO: insert code here */
+            remove(data, curNode.left);
+
+        }
+        // if item I want to remove is larger than the data of the current node...
+        else if (data.compareTo(curNode.data) > 0) {
+	    // recursively call remove on RIGHT subtree
+	    /* TODO: insert code here */
+            remove(data, curNode.right);
+            
+        }
+	/* Found item to remove, time to remove */
+        else { 
+	    /* Case 1: node is a leaf, return null */
+            if(curNode.left == null && curNode.right == null) {
+                return null;
+            }
+            
+	    /* Case 2A: else if node only has a left child, then return left child */
+            if(curNode.left != null && curNode.right == null) {
+                return curNode.left;
+            }
+
+	    /* Case 2B: else if node only has a right child, then return right child */
+             else if(curNode.right != null && curNode.left == null) {
+                return curNode.right;
+            }
+            /* Case 3: else (node has 2 children) so:           
+               UNCOMMENT CODE BELOW AND PUT IN APPROPRIATE SPOT
+
+                T toDel = findMax(curNode.left);  // find inorder predecessor (max of left child)
+                curNode.data = toDel;		  // copy value from inorder predecessor to node
+                curNode.left = remove(toDel, curNode.left);  // delete the inorder predecessor
+                return curNode;
+            */
+
+
+            else {
+                T toDel = findMax(curNode.left);  // find inorder predecessor (max of left child)
+                curNode.data = toDel;		  // copy value from inorder predecessor to node
+                curNode.left = remove(toDel, curNode.left);  // delete the inorder predecessor
+                return curNode;
+            }
+        }
+        return curNode;
+    }
+
+
+}
